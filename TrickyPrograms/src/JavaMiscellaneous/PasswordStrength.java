fb2c1db42e39b0814ed810ed6f813525beee6344
iamni <iamni@iamnisar812>
2024-04-27 18:40:36 +0530

    27 apr c1

---
+++ TrickyPrograms/src/JavaMiscellaneous/PasswordStrength.java
@@ -0,0 +1,53 @@
+package JavaMiscellaneous;
+
+public class PasswordStrength {
+
+	public static void main(String[] args) {
+		String password = "NIisIa@bhbdch222222";
+
+		passwordStrength(password);
+
+		// length should be 16
+		// 2 uppercase
+		// 2 lowercase
+		// 1 special character
+
+	}
+
+	public static boolean passwordStrength(String password) {
+		int upperCaseCount = 0;
+		int lowerCaseCount = 0;
+		int digitsCount = 0;
+		int specialCharsCount = 0;
+
+		boolean passwordStrength;
+
+		for (int i = 0; i < password.length(); i++) {
+			char ch = password.charAt(i);
+			if (ch >= 'A' && ch <= 'Z') {
+				upperCaseCount++;
+			} else if (ch >= 'a' && ch <= 'z') {
+				lowerCaseCount++;
+			} else if (ch >= '0' && ch <= '9') {
+				digitsCount++;
+			} else {
+				specialCharsCount++;
+			}
+		}
+
+		System.out.println("Uppercase Count: " + upperCaseCount);
+		System.out.println("Lowercase Count: " + lowerCaseCount);
+		System.out.println("Digits Count: " + digitsCount);
+		System.out.println("Special Characters Count: " + specialCharsCount);
+
+		if (upperCaseCount >= 2 && lowerCaseCount >= 2 && digitsCount >= 2 && specialCharsCount == 1
+				&& password.length() >= 16) {
+			passwordStrength = true;
+		} else {
+			passwordStrength = false;
+		}
+		System.out.println("Password Strength: " + passwordStrength);
+		return passwordStrength;
+	}
+
+}
---
+++ TrickyPrograms/src/JavaMiscellaneous/PatternPrint.java
@@ -0,0 +1,26 @@
+package JavaMiscellaneous;
+
+import java.util.Arrays;
+
+public class PatternPrint {
+
+	public static void main(String[] args) {
+
+		int[][] pattern = new int[4][4];
+
+		for (int i = 0; i <= 3; i++) {
+			for (int j = 0; j <= 3; j++) {
+				if (i == j) {
+					pattern[i][j] = i + 1;
+				} else if (j == 3) {
+					pattern[i][j] = i + 1;
+				} else {
+					pattern[i][j] = 0;
+				}
+			}
+		}
+		for (int[] r : pattern) {
+			System.out.println(Arrays.toString(r));
+		}
+	}
+}
