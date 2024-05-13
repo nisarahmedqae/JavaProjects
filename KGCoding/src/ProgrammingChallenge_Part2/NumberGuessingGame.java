9cd56e0242bfa740b12a992dbbb9f0dc89630198
Nisar Ahmed <nisarahmed.qae@gmail.com>
2024-05-13 21:18:02 +0530

    13 may c5

--- KGCoding/src/ProgrammingChallenge_Part2/NumberGuessingGame.java
+++ KGCoding/src/ProgrammingChallenge_Part2/NumberGuessingGame.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class NumberGuessingGame {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
--- KGCoding/src/ProgrammingChallenge_Part2/PasswordChecker.java
+++ KGCoding/src/ProgrammingChallenge_Part2/PasswordChecker.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class PasswordChecker {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Set your password\n");
