2ded556e97fc95ee2acba613d8cb51c2be674bfe
Nisar Ahmed <nisarahmed.qae@gmail.com>
2024-05-19 16:09:00 +0530

    19 may c3

--- AmazonWebFramework/src/test/java/nisarahmedqae/tests/ScrollingTest.java
+++ AmazonWebFramework/src/test/java/nisarahmedqae/tests/ScrollingTest.java
@@ -1,4 +1,4 @@
-package nahmed;
+package nisarahmedqae.tests;
 
 import java.time.Duration;
 
@@ -21,21 +21,8 @@ public class ScrollingTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 
-		// Find the element for Switzerland
-		WebElement switzerland = driver.findElement(By.xpath("//a[@title='Switzerland']"));
-		System.out.println("Switzerland Location: " + switzerland.getLocation());
-
-		// Scroll to the Switzerland element using scrollIntoView
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", switzerland);
-		Thread.sleep(3000);
-
 		// Scroll by 200 pixels horizontally and 0 pixels vertically
-		((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 3000)");
-		Thread.sleep(3000);
-
-		// Scroll to the element with id 'cite_note-230'
-		WebElement ele200 = driver.findElement(By.xpath("//li[@id='cite_note-230']"));
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", ele200);
+		((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 4000)");
 		Thread.sleep(3000);
 
 		// Scroll to the top of the page (coordinates: 0,0)
