ffec48574ea28a606290be9a8b0566609eab313e
Nisar Ahmed <nisarahmed.qae@gmail.com>
2024-08-18 16:23:48 +0530

    18 aug c1

--- SeleniumFramework_TMB/src/test/java/com/tmb/tests/HomePageTests.java
+++ SeleniumFramework_TMB/src/test/java/com/tmb/tests/HomePageTests.java
@@ -1,7 +1,13 @@
 package com.tmb.tests;
 
+import java.time.Duration;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.tmb.driver.DriverManager;
@@ -10,10 +16,32 @@ public final class HomePageTests extends BaseTest {
 	private HomePageTests() {
 
 	}
+	/*
+	 * validate whether title containing "google search"
+	 * validate whether title is not null and the length of title greater than 15 and less than 100
+	 * check for the links in the pages --> testing mini byte - youtube
+	 * number of links displayed is exactly 10 or 15
+	 */
 
 	@Test
-	public void test2() {
-		DriverManager.getDriver().findElement(By.name("q")).sendKeys("Testing Mini Byte", Keys.ENTER);
+	public void test2() throws InterruptedException {
+		DriverManager.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		DriverManager.getDriver().findElement(By.name("q")).sendKeys("Testing Mini Bytes YouTube", Keys.ENTER);
+		Thread.sleep(5);
+		String title = DriverManager.getDriver().getTitle();
+		System.out.println(title);
+		System.out.println(title.length());
+		
+		assertThat(title)
+		.as("Object is actually null").isNotNull()
+		.as("It does not contain expected text").containsIgnoringCase("google search")
+		.hasSizeBetween(15, 100);
+		
+		List<WebElement> elements = DriverManager.getDriver().findElements(By.xpath("//div[contains(@class,'notranslate')]//span[@class='VuuXrf']"));
+		assertThat(elements)
+		.hasSizeGreaterThan(10)
+		.extracting(WebElement :: getText)
+		.contains("Testing Mini Bytes");
 	}
 
 }
