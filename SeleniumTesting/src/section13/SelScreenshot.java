6edc85f2b8c6ee6bf56a30c2d1e2cd3ce6ffb65d
Nisar Ahmed <nisarahmed.qae@gmail.com>
2025-01-05 15:46:16 +0530

    jan 5 c1

--- SeleniumTesting/src/section13/SelScreenshot.java
+++ SeleniumTesting/src/section13/SelScreenshot.java
@@ -1,9 +1,11 @@
 package section13;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Base64;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
@@ -23,15 +25,7 @@ public class SelScreenshot {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
-		// maximize window
 		driver.manage().window().maximize();
-
-		// delete all cookies
-		driver.manage().deleteAllCookies();
-
-		// delete specific cookie
-		driver.manage().deleteCookieNamed("cookie name mention here");
 
 		// Screenshot of: Full Page
 		File pageScreenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
@@ -40,19 +34,32 @@ public class SelScreenshot {
 		// Screenshot of: Element
 		WebElement searchElement = driver.findElement(By.className("SDkEP"));
 		File elementScreenshotFile = searchElement.getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(elementScreenshotFile, new File("C:\\WebDrivers\\SeleniumScreenshots\\ElementScreenshot.png"));
+		FileUtils.copyFile(elementScreenshotFile,
+				new File("C:\\WebDrivers\\SeleniumScreenshots\\ElementScreenshot.png"));
 
-		
 		// Using timestamp while Capturing Screenshots
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        String timestamp = dateFormat.format(new Date());
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+		String timestamp = dateFormat.format(new Date());
 
-        FileUtils.copyFile(pageScreenshotFile, new File("C:\\WebDrivers\\SeleniumScreenshots\\" + timestamp + ".png"));
+		FileUtils.copyFile(pageScreenshotFile, new File("C:\\WebDrivers\\SeleniumScreenshots\\" + timestamp + ".png"));
 
-        // Screenshot using Shutterbug library
-     	Shutterbug.shootPage(driver,Capture.FULL_SCROLL,true).save("C:\\WebDrivers\\SeleniumScreenshots\\");
-     		
-		
+		// Screenshot using Shutterbug library
+		Shutterbug.shootPage(driver, Capture.FULL_SCROLL, true).save("C:\\WebDrivers\\SeleniumScreenshots\\");
+
+		// Screenshot as Base64 string
+		String screenshotBase64 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+		System.out.println("Screenshot as Base64 String: " + screenshotBase64);
+
+		// Decode Base64 to byte array
+		byte[] decodedBytes = Base64.getDecoder().decode(screenshotBase64);
+
+		// Save the bytes to a .jpeg file
+		File jpegFile = new File("C:\\WebDrivers\\SeleniumScreenshots\\screenshotBase64.jpeg");
+		try (FileOutputStream fos = new FileOutputStream(jpegFile)) {
+			fos.write(decodedBytes);
+			System.out.println("Screenshot saved as JPEG at: " + jpegFile.getAbsolutePath());
+		}
+
 		driver.quit();
 	}
 
